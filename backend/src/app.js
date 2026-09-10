09ea72b2cc234311149481f524b62efb9729f66d
RPH07 <renaldyputrahasli@gmail.com>
2026-09-10 22:58:57 +0700

    feat: serve static files for admin interface from public directory

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const projectsRoutes = require("./routes/projects.routes");
 const authRoutes = require("./routes/auth.routes");
 const uploadRoutes = require("./routes/upload.routes");
+const path = require("path");
 
 const app = express();
 
@@ -11,6 +12,7 @@ app.get('/health', (req, res) => {res.json({status: 'ok'});});
 app.use('/auth', authRoutes);
 app.use('/projects', projectsRoutes);
 app.use('/upload', uploadRoutes);
+app.use('/admin', express.static(path.join(__dirname, '../public')));
 
 
 module.exports = app;
